afd0c1976faa487519023552507d85ae69607eba
dezsiszabi <dezsiszabi88@gmail.com>
2022-02-01 21:26:04 -0500

    Remove leftover struct

--- cnx.h
+++ cnx.h
@@ -4,18 +4,6 @@
 #include <ldap.h>
 #include <uv.h>
 
-struct ldap_cnx
-{
-  LDAP *ld;
-  ldap_conncb *ldap_callback;
-  const char *sasl_mechanism;
-  uv_poll_t *handle;
-  napi_async_context async_context;
-  napi_ref reconnect_callback_ref, disconnect_callback_ref, callback_ref;
-  napi_ref this_ref;
-  napi_env env;
-};
-
 class LDAPCnx : public Napi::ObjectWrap<LDAPCnx>
 {
 public:
